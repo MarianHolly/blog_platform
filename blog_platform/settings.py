8ae2c9a121f00ff2d0c93ff00bb38a7e3dc54831
Stupel <marian.holly7117@gmail.com>
2025-04-07 21:02:00 +0200

    Set up .env and requirements files

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -10,17 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from dotenv import load_dotenv
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m!w*jfpwo7=*6^w!qfg&ves*2pb_3#5naca8f&+6if+5$vm7ob"
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
