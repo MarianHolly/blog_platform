115f2155054e5919747c7d096f559673e6331a02
Stupel <marian.holly7117@gmail.com>
2025-04-10 18:08:59 +0200

    Profile model updated, Subscription model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
-from django.db.models import CASCADE, OneToOneField, Model
-from django.db.models.fields import CharField, TextField
-
-from content.models import Article
+from django.db.models import CASCADE, Model, OneToOneField, ImageField, ForeignKey
+from django.db.models.fields import CharField, TextField, DateTimeField
 
 
 # Extending existing User model
@@ -15,11 +13,26 @@ class Profile(Model):
 
     user = OneToOneField(User, on_delete=CASCADE)
     role = CharField(max_length=20, choices=USER_ROLES, default='reader')
+    biography = TextField(max_length=500, null=True, blank=True)
+    avatar = ImageField(default='default_avatar.png', upload_to='profile_pics')
 
-    biography = TextField(null=True, blank=True)
+    class Meta:
+        ordering = ['user__username']
 
     def __repr__(self):
-        return f"Profile(name={self.user.username}, role={self.role})"
+        return f"Profile(name={self.user}, role={self.role})"
 
     def __str__(self):
         return f"{self.user.username}"
+
+
+class Subscription(Model):
+    subscriber = ForeignKey(User, on_delete=CASCADE, related_name='subscriptions')
+    writer = ForeignKey(User, on_delete=CASCADE, related_name='subscribers')
+    created = DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return f"Subscription(subscriber={self.subscriber}, writer={self.writer})"
+
+    def __str__(self):
+        return f"{self.subscriber.username} subscribed to {self.writer}"
