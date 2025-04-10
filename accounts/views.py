135b0239b9843818f912f83e7f0baf9f29f6f72f
Stupel <marian.holly7117@gmail.com>
2025-04-10 20:36:00 +0200

    custom SignUp template

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ from accounts.models import Profile
 
 # Create your views here.
 class SignUpView(CreateView):
-    template_name = "form.html"
+    template_name = "registration/signup.html"
     form_class = SignUpForm
     success_url = reverse_lazy('home')
 
