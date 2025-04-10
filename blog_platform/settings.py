15cbc1fb02949150195025223cbbcb6a6ca0aacf
Stupel <marian.holly7117@gmail.com>
2025-04-10 20:36:35 +0200

    register django-crispy-form

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -38,9 +38,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "crispy_forms",
+    "crispy_tailwind",
+
     "accounts",
     "content",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
