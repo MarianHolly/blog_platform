6f4d12aa188f44b0bb859c9abd2680ac6a24bf02
Stupel <marian.holly7117@gmail.com>
2025-04-14 11:32:01 +0200

    Form for Create and Update Articles

---
+++ content/forms.py
@@ -0,0 +1,21 @@
+from django.forms import RadioSelect, TextInput, Textarea
+from django.forms.models import ModelForm
+
+from content.models import Article
+
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'subtite', 'description', 'content', 'bulletin', 'status', 'visibility']
+        widgets = {
+            'status': RadioSelect,
+            'visibility': RadioSelect,
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'subtite': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'w-full', 'rows': 3}),
+            'content': Textarea(attrs={'class': 'w-full'})
+        }
+
+    #This form will be accessible only for user with role 'writer'
+    #TODO: User is provided, take bulletin from user.profile.bulletin
