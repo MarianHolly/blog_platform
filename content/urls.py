52f1d27050fb830d011ea1cfd0deed65f871c514
Stupel <marian.holly7117@gmail.com>
2025-04-14 11:32:50 +0200

    Create Urls and Vies

--- content/urls.py
+++ content/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from content.views import HomePageView, AboutPageView, ArticleListView, ArticleDetailView
+from content.views import HomePageView, AboutPageView, ArticleListView, ArticleDetailView, ArticleUpdateView, \
+    ArticleCreateView, ArticleDeleteView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("articles/", ArticleListView.as_view(), name="article_list"),
-    path("article/<int:pk>", ArticleDetailView.as_view(), name="article_detail"),
+    path("article/<int:pk>/", ArticleDetailView.as_view(), name="article_detail"),
+    path("article/create/", ArticleCreateView.as_view(), name="article_create"),
+    path("article/edit/<int:pk>/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("article/delete/<int:pk>/", ArticleDeleteView.as_view(), name='article_delete'),
+
 ]
--- content/views.py
+++ content/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView, TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, TemplateView, CreateView, UpdateView, DeleteView
 
+from content.forms import ArticleForm
 from content.models import Article
 
 
 # ==================================== BLOG PLATFORM ======================== #
 
 class HomePageView(ListView):
-    template_name = 'content/home.html'
+    template_name = "content/home.html"
     model = Article
     context_object_name = "articles"
 
 
 class AboutPageView(TemplateView):
-    template_name = 'content/about.html'
+    template_name = "content/about.html"
 
 
 class ArticleListView(ListView):
@@ -23,8 +25,25 @@ class ArticleListView(ListView):
 
 
 class ArticleDetailView(DetailView):
-    template_name = "content/article.html"
+    template_name = "content/article_detail.html"
     model = Article
     context_object_name = "article"
 
 
+class ArticleCreateView(CreateView):
+    template_name = "content/form.html"
+    form_class = ArticleForm
+    success_url = reverse_lazy("article_list")
+
+
+class ArticleUpdateView(UpdateView):
+    template_name = "content/form.html"
+    form_class = ArticleForm
+    model = Article
+    success_url = reverse_lazy("article_list")
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = "content/confirm_delete.html"
+    model = Article
+    success_url = reverse_lazy('article_list')
