b67f4be599c88c3727f3724657245482846cf84f
Stupel <marian.holly7117@gmail.com>
2025-04-28 17:24:28 +0200

    add Subscription to admin panel

--- content/admin.py
+++ content/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from content.models import Article, Bulletin
+from content.models import Article, Bulletin, Subscription
 
 # Register your models here.
 admin.site.register(Article)
 admin.site.register(Bulletin)
+admin.site.register(Subscription)
