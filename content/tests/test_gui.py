bdbdc5dd6a2e3ce353dfecf44fff115fe4eac9b9
Stupel <marian.holly7117@gmail.com>
2025-05-10 21:16:32 +0200

    gui testing for SingUp

---
+++ content/tests/test_gui.py
@@ -0,0 +1,51 @@
+import time
+
+from django.test import TestCase
+
+from unittest import skip
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+class GuiTestWithSelenium(TestCase):
+    @skip
+    def test_page_titles(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://127.0.0.1:8000/")
+        assert "Blog Platform" in self.driver.title
+        time.sleep(1)
+        self.driver.get("http://127.0.0.1:8000/about/")
+        assert "O Platforme" in self.driver.title
+        time.sleep(1)
+        self.driver.get("http://127.0.0.1:8000/qa/")
+        assert "Q & A" in self.driver.title
+
+    def test_signup(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("http://127.0.0.1:8000/accounts/signup/")
+        time.sleep(1)
+        username_input = self.driver.find_element(by=By.NAME, value="username")
+        username_input.send_keys("SlavojZizek")
+        time.sleep(1)
+        firstname_input = self.driver.find_element(by=By.NAME, value="first_name")
+        firstname_input.send_keys("Slavoj")
+        time.sleep(1)
+        lastname_input = self.driver.find_element(by=By.NAME, value="last_name")
+        lastname_input.send_keys("Žižek")
+        time.sleep(1)
+        email_input = self.driver.find_element(by=By.NAME, value="email")
+        email_input.send_keys("slavojzizek@mail.io")
+        time.sleep(1)
+        password1_input = self.driver.find_element(by=By.NAME, value="password1")
+        password1_input.send_keys("ZPc5dash32u0")
+        time.sleep(1)
+        password2_input = self.driver.find_element(by=By.NAME, value="password2")
+        password2_input.send_keys("ZPc5dash32u0")
+        time.sleep(3)
+        submit_btn = self.driver.find_element(By.ID, 'id_submit')
+        submit_btn.send_keys(Keys.RETURN)
+
+        assert ("Prihláste sa do svojho účtu" or "Toto užívateľské meno je už obsadené." in self.driver.page_source)
+
+
+
