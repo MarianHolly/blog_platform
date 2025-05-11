2327e24db10b1eb7046ae5a12e94ca5f80321361
Stupel <marian.holly7117@gmail.com>
2025-05-11 15:27:17 +0200

    CommentModelForm

---
+++ engagement/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm, Textarea
+
+from engagement.models import Comment
+
+
+class CommentModelForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        labels = {
+            'content': 'Zapoj sa do diskusie:'
+        }
+        widgets = {
+            'content': Textarea(attrs={'placeholder': 'Vyjadri svoj názor...'}),
+        }
+
+
