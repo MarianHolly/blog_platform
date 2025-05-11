5134b08f28c9830f054df525c34bf65caccdc3f3
Stupel <marian.holly7117@gmail.com>
2025-05-11 13:09:23 +0200

    models for Like and Comment

--- engagement/admin.py
+++ engagement/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from engagement.models import Like, Comment
+
 # Register your models here.
+admin.site.register(Comment)
+admin.site.register(Like)
--- engagement/models.py
+++ engagement/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.db.models import Model, DateTimeField, ForeignKey, TextField, CASCADE
+
+from accounts.models import Profile
+from content.models import Article
+
 
 # Create your models here.
+class Comment(Model):
+    author = ForeignKey(Profile, on_delete=CASCADE)
+    article = ForeignKey(Article, on_delete=CASCADE)
+    content = TextField(max_length=500, blank=False, null=False)
+
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"Comment(author={self.author}, article={self.article})"
+
+    def __srt__(self):
+        return f"{self.author} commented on {self.article}"
+
+
+class Like(Model):
+    author = ForeignKey(Profile, on_delete=CASCADE)
+    article = ForeignKey(Article, on_delete=CASCADE)
+    created = DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return f"Like(author={self.author}, article={self.article})"
+
+    def __srt__(self):
+        return f"{self.author} liked {self.article}"
