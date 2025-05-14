35c386e93d16f1fd77a3087eff00122f922bb682
Stupel <marian.holly7117@gmail.com>
2025-05-14 15:19:52 +0200

    Like functionality in LikeToggleView

--- engagement/views.py
+++ engagement/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+
+from content.models import Article
+from engagement.models import Like
+
 
 # Create your views here.
+class LikeToggleView(LoginRequiredMixin, View):
+    def post(self, request, id):
+        # get article
+        article = get_object_or_404(Article, id=id)
+        user_profile = request.user.profile
+
+        # check if user is not author of article
+        if article.author == user_profile:
+            print('You are author')
+            messages.warning(request, "Nemôžeš dať like vlastnému článku.")
+            next_url = request.POST.get('next', '')
+            if next_url:
+                return HttpResponseRedirect(next_url)
+            return redirect(article.get_absolute_url())
+
+        # if 'like' exists
+        like = Like.objects.filter(article=article, author=user_profile)
+
+        # toggle state of 'like'
+        if like.exists():
+            like.delete()
+            messages.success(request, 'Odobral si like tomuto článku.')
+            print('Like deleted')
+
+        else:
+            Like.objects.create(article=article, author=user_profile)
+            messages.success(request, 'Dal si like tomuto článku.')
+            print('Like created')
+
+        next_url = request.POST.get('next', '')
+        if next_url:
+            return HttpResponseRedirect(next_url)
+        return redirect('article_detail', id=article.id )
