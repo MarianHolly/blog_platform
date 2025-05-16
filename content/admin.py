b2e6f70ed9b696f7c3a7c122e6599fe5f1859f01
Stupel <marian.holly7117@gmail.com>
2025-05-16 22:54:16 +0200

    custom admin page

--- content/admin.py
+++ content/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, TabularInline
 
 from content.models import Article, Bulletin, Subscription
 
+
+class ArticleAdmin(ModelAdmin):
+    list_display = ['title', 'bulletin', 'author']
+    list_filter = ['status', 'bulletin']
+    search_fields = ['title']
+    readonly_fields = ['created', 'updated']
+
+
+class BulletinAdmin(ModelAdmin):
+    list_display = ['title', 'owner', 'articles_count', 'subscriber_count']
+    readonly_fields = ['created', 'updated']
+
+    def subscriber_count(self, obj):
+        return obj.subscribers.count()
+    subscriber_count.short_description = 'Subscribers'
+
+    def articles_count(self, obj):
+        return obj.articles.count()
+    articles_count.short_description = 'Articles'
+
+
+class SubscriptionAdmin(ModelAdmin):
+    list_display = ['bulletin', 'subscriber']
+
 # Register your models here.
-admin.site.register(Article)
-admin.site.register(Bulletin)
-admin.site.register(Subscription)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Bulletin, BulletinAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
--- engagement/admin.py
+++ engagement/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from engagement.models import Like, Comment, ReadLater
 
+
+class CommentAdmin(ModelAdmin):
+    list_display = ['author', 'article', 'content']
+
+
+class LikeAdmin(ModelAdmin):
+    list_display = ['author', 'article']
+
+
+class ReadLaterAdmin(ModelAdmin):
+    list_display = ['author', 'article']
+
+
 # Register your models here.
-admin.site.register(Comment)
-admin.site.register(Like)
-admin.site.register(ReadLater)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(ReadLater, ReadLaterAdmin)
