614d31e6e0314fe24aef21cf21774a9d7582d9a7
Stupel <marian.holly7117@gmail.com>
2025-05-16 22:16:33 +0200

    register 'ReadLater' to urls and admin

--- engagement/admin.py
+++ engagement/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from engagement.models import Like, Comment
+from engagement.models import Like, Comment, ReadLater
 
 # Register your models here.
 admin.site.register(Comment)
 admin.site.register(Like)
+admin.site.register(ReadLater)
--- engagement/urls.py
+++ engagement/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from engagement.views import LikeToggleView
+from engagement.views import LikeToggleView, ReadLaterToggleView
 
 urlpatterns = [
     path("like/<int:id>/", LikeToggleView.as_view(), name="toggle_like"),
+    path("read-later/<int:id>/", ReadLaterToggleView.as_view(), name="toggle_read_later"),
 ]
