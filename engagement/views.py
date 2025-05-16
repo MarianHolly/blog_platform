6259c71ed1fbe2d97de970a58324c1cab013c677
Stupel <marian.holly7117@gmail.com>
2025-05-16 20:36:49 +0200

    ReadLaterToggleView to manage 'read later'

--- engagement/views.py
+++ engagement/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
 
 from content.models import Article
-from engagement.models import Like
+from engagement.models import Like, ReadLater
 
 
 # Create your views here.
@@ -17,7 +17,6 @@ class LikeToggleView(LoginRequiredMixin, View):
 
         # check if user is not author of article
         if article.author == user_profile:
-            print('You are author')
             messages.warning(request, "Nemôžeš dať like vlastnému článku.")
             next_url = request.POST.get('next', '')
             if next_url:
@@ -39,3 +38,30 @@ class LikeToggleView(LoginRequiredMixin, View):
         if next_url:
             return HttpResponseRedirect(next_url)
         return redirect('article_detail', id=article.id )
+
+
+class ReadLaterToggleView(LoginRequiredMixin, View):
+    def post(self, request, id):
+        article = get_object_or_404(Article, id=id)
+        user_profile = request.user.profile
+
+        if article.author == user_profile:
+            message.warning(request, "Nemôžeš označiť svoj článok ako prečítať neskôr.")
+            next_url = request.POST.get('next', '')
+            if next_url:
+                return HttpResponseRedirect(next_url)
+            return redirect(article.get_absolute_url())
+
+        read_later = ReadLater.objects.filter(article=article, author=user_profile)
+
+        if read_later.exists():
+            read_later.delete()
+            messages.success(request, 'Článok už nie je uložený na prečítanie.')
+        else:
+            ReadLater.objects.create(article=article, author=user_profile)
+            messages.success(request, 'Článok je uložený na prečítanie neskôr.')
+
+        next_url = request.POST.get('next', '')
+        if next_url:
+            return HttpResponseRedirect(next_url)
+        return redirect(article.get_absolute_url())
