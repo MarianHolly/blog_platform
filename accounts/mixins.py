f074050161d9479c0bf00c7a9e27180520bf6cb6
Stupel <marian.holly7117@gmail.com>
2025-05-18 21:20:07 +0200

    mixins created and updated

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -4,3 +4,13 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 class ReaderRequiredMixin(UserPassesTestMixin):
     def test_func(self):
         return self.request.user.is_authenticated and self.request.user.profile.role == 'reader'
+
+
+class WriterRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.profile.role == 'writer'
+
+
+class AdministratorRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.profile.role == 'admin'
--- content/mixins.py
+++ content/mixins.py
@@ -1,11 +1,6 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 
-class WriterRequiredMixin(UserPassesTestMixin):
-    def test_func(self):
-        return self.request.user.is_authenticated and self.request.user.profile.role == 'writer'
-
-
 class ArticleOwnerMixin(UserPassesTestMixin):
     def test_func(self):
         article = self.get_object()
