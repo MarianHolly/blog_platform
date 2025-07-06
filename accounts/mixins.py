d1ef54b79336c77fdf8cc19ae083dd17fb703b4e
Stupel <marian.holly7117@gmail.com>
2025-07-06 16:51:10 +0200

    account mixins expanded

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -1,14 +1,18 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.core.exceptions import PermissionDenied
+
 
 class ReaderRequiredMixin(UserPassesTestMixin):
     def test_func(self):
         return self.request.user.is_authenticated
 
     def handle_no_permission(self):
-        messages.error(self.request, 'Musíte byť prihlásený.')
-        return redirect('login')
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, 'Musíte byť prihlásený.')
+            return redirect('login')
+        raise PermissionDenied('Musíte byť prihlásený.')
 
 
 class WriterRequiredMixin(UserPassesTestMixin):
@@ -22,10 +26,34 @@ class WriterRequiredMixin(UserPassesTestMixin):
             return False
 
     def handle_no_permission(self):
-        if self.request.user.is_authenticated:
-            messages.error(self.request, 'Potrebujete autorské oprávnenia.')
-            return redirect('profile', username=self.request.user.username)
-        return redirect('login')
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, 'Musíte byť prihlásený.')
+            return redirect('login')
+        # Return 403 for authenticated users without permission
+        raise PermissionDenied('Potrebujete autorské oprávnenia.')
+
+
+class WriterOrSuperAdminRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        user = self.request.user
+        if not user.is_authenticated:
+            return False
+
+        # Allow superusers
+        if user.is_superuser:
+            return True
+
+        # Allow writers
+        try:
+            return user.profile.is_writer
+        except:
+            return False
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, 'Musíte byť prihlásený.')
+            return redirect('login')
+        raise PermissionDenied('Potrebujete autorské alebo administrátorské oprávnenia.')
 
 
 class AdministratorRequiredMixin(UserPassesTestMixin):
@@ -39,7 +67,7 @@ class AdministratorRequiredMixin(UserPassesTestMixin):
             return False
 
     def handle_no_permission(self):
-        if self.request.user.is_authenticated:
-            messages.error(self.request, 'Potrebujete administrátorské oprávnenia.')
-            return redirect('profile', username=self.request.user.username)
-        return redirect('login')
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, 'Musíte byť prihlásený.')
+            return redirect('login')
+        raise PermissionDenied('Potrebujete administrátorské oprávnenia.')
