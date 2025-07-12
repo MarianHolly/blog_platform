c08b315878ec5e1d6d4f69f267087e3586ff06c0
Stupel <marian.holly7117@gmail.com>
2025-07-12 22:00:46 +0200

    celery removed

--- blog_platform/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_platform.settings')
-
-celery_app = Celery('blog_platform')
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-celery_app.autodiscover_tasks()
