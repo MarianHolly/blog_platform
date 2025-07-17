192f5b89719e433a0d945194311074b99f5c628d
Stupel <marian.holly7117@gmail.com>
2025-07-17 19:58:16 +0200

    deleting testing form version

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -104,7 +104,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "csp.middleware.CSPMiddleware",
+    "csp.middleware.CSPMiddleware",
 ]
 
 # Security headers
