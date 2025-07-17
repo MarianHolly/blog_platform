6bbf8c386bc9f255b48bf16a873b4539b18a8bc9
Stupel <marian.holly7117@gmail.com>
2025-07-17 18:29:17 +0200

    migrating CKEditor to newer, safer version

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("", include("content.urls")),
     path("accounts/", include("accounts.urls") ),
     path('', include('engagement.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
 
 if settings.DEBUG:
