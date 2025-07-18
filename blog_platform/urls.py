88c706a30f7441b088fd69ad13f2445e3e834c28
Stupel <marian.holly7117@gmail.com>
2025-07-18 10:01:53 +0200

    reverse changes

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path("accounts/", include("accounts.urls") ),
     path('', include('engagement.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
