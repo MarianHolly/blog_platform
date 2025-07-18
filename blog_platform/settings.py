3127c5c0493f99def2b4ce4adc97a718fba911a2
Stupel <marian.holly7117@gmail.com>
2025-07-18 10:01:53 +0200

    reverse changes

--- blog_platform/settings.py
+++ blog_platform/settings.py
@@ -108,22 +108,11 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_MAX_AGE = 31536000
 
 # Security headers
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
-
-CONTENT_SECURITY_POLICY = {'DIRECTIVES': {'connect-src': ("'self'",),
-                'default-src': ("'self'",),
-                'font-src': ("'self'", 'data:'),
-                'frame-src': ('*',),
-                'img-src': ("'self'", '*', 'data:'),
-                'script-src': ("'self'",),
-                'style-src': ("'self'", "'unsafe-inline'")}}
-
 
 # Only enable HTTPS redirects in production, not in development
 if not DEBUG:
@@ -250,10 +239,6 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Static files
 STATIC_URL = "/static/"
