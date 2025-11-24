3ce5e1452fa8f9cf2d7d8b3f9bc54ed673aefe9a
Marián Hollý <marian.holly7117@gmail.com>
2025-11-24 21:20:50 +0100

    update model in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -52,3 +52,52 @@ class Profile(Model):
     @property
     def is_admin(self):
         return self.role == 'admin'
+
+    def get_articles_count(self):
+        """Get count of articles written by this user (if writer)."""
+        if self.is_writer and hasattr(self, 'bulletin'):
+            return self.bulletin.articles.count()
+        return 0
+
+    def get_likes_received_count(self):
+        """Get count of likes received on this user's articles."""
+        from content.models import Article
+        from engagement.models import Like
+        if self.is_writer and hasattr(self, 'bulletin'):
+            articles = Article.objects.filter(bulletin=self.bulletin)
+            return Like.objects.filter(article__in=articles).count()
+        return 0
+
+    def get_comments_received_count(self):
+        """Get count of comments received on this user's articles."""
+        from content.models import Article
+        from engagement.models import Comment
+        if self.is_writer and hasattr(self, 'bulletin'):
+            articles = Article.objects.filter(bulletin=self.bulletin)
+            return Comment.objects.filter(article__in=articles).count()
+        return 0
+
+    def get_articles_liked_count(self):
+        """Get count of articles liked by this user (if reader)."""
+        from engagement.models import Like
+        return Like.objects.filter(user=self.user).count()
+
+    def get_comments_posted_count(self):
+        """Get count of comments posted by this user (if reader)."""
+        from engagement.models import Comment
+        return Comment.objects.filter(author=self.user).count()
+
+    def get_subscriptions_count(self):
+        """Get count of bulletins this user is subscribed to."""
+        from content.models import Subscription
+        return Subscription.objects.filter(subscriber=self).count()
+
+    def get_bulletin_info(self):
+        """Get bulletin information if user is a writer."""
+        if self.is_writer and hasattr(self, 'bulletin'):
+            return {
+                'title': self.bulletin.title,
+                'articles': self.bulletin.articles.count(),
+                'subscribers': self.bulletin.subscribers.count(),
+            }
+        return None
