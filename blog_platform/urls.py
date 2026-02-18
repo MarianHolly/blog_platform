0a00f520ebbbfcf583608c031e7d1ed8010a6d81
Marián Hollý <97178172+MarianHolly@users.noreply.github.com>
2026-02-18 18:32:20 +0100

    Add /health/ endpoint

--- blog_platform/urls.py
+++ blog_platform/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from content.views import *
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 # Customize Django Admin Site
 admin.site.site_header = "Blog Platform Admin"
